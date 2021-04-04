c33e0070d27f3e0a814589881694cee19bfe3fe7
mlscmoulika <53378071+mlscmoulika@users.noreply.github.com>
2021-04-04 15:40:32 +0530

    added upload file- add uploads folder in your htdocs

--- signup.php
+++ signup.php
@@ -4,6 +4,13 @@
 <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Customer Register</title>
+    <style>
+    body{
+        background-image: url("train.jpg");
+        background-size: 100%;
+    background-attachment: fixed;
+    }
+    </style>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='style1.css'>
     <link rel='stylesheet' type='text/css' media='screen' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'> 
@@ -18,29 +25,32 @@
          }
       </script>
 </head>  
-<body style=" color: yellow; background-image:url(train.jpg);background-size: 100%;
-    background-attachment: fixed;"> 
+<body style=" color: yellow;);"> 
     <br> <br>
     <center><h1 style="color:white;"><u> TRAIN TICKET RESERVATION SYSTEM </u></h1></center>
     <center><h2 style="color:white;">User Registration Form</h2></center>
-    <form action="" method="POST">  
+    <form action="" method="POST" enctype="multipart/form-data">  
     <div class="booking-form-box" style="border: 0px solid black;">
     <div class="booking-form"  style=" background: rgba(0,0,0,0.5); width:490px;">
         <label>Username</label><br>
-        <input type="text" name="emp" placeholder="username"  required><br>
+        <input type="text" name="emp" onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)" placeholder="username" required><br>
+        <!-- <input type="text" name="emp" placeholder="username" required><br> -->
 
         <label>Email-id</label><br>
         <input type="email" name="emailid" placeholder="email-id" id="email" required><br>
         <!-- <input type="email" class="input-field" placeholder="Email Id" name="emailid" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Sorry, only letters, numbers, periods(.) and '@' are allowed."><br> -->
         <label>phone</label><br>
-        <input type="text" name="phone" placeholder="phone" onkeypress='return restrictAlphabets(event)' pattern="\d{10}" maxlength="10"><br>
+        <input type="text" name="phone" placeholder="phone" onkeypress='return restrictAlphabets(event)' pattern="\d{10}" maxlength="10" required><br>
 
         <label>Age</label><br>
-        <input type="number" name="age" placeholder="age" min="0" max="120" oninput="validity.valid||(value='');" required><br>
+        <input type="number" name="age" placeholder="age" min="18" max="120" oninput="validity.valid||(value='');" required><br>
 
         <label>Aadhar Number</label><br>
         <input type="text" name="Aadhar_No" placeholder="aadhar number" onkeypress='return restrictAlphabets(event)' pattern="\d{12}" maxlength="12" required><br>
 
+        <label>Upload your identity information</label><br>
+        <input type="file" name="file" required><br>
+        
         <label>Address</label><br>
         <input type="text" name="address"placeholder="address" required><br>
 
@@ -70,8 +80,10 @@
 <?php  
     if(isset($_POST["submit"]))
     {  
+        echo "hello";
         if(!empty($_POST['emp']) && !empty($_POST['emailid']) && !empty($_POST['phone'])&& !empty($_POST['emppass']) && !empty($_POST['empconf']) && !empty($_POST['age'])&& !empty($_POST['address'])&& !empty($_POST['gender'])&& !empty($_POST['Aadhar_No']))
-        {  
+        { 
+            echo "hi"; 
             $emp=$_POST['emp'];
             $emailid=$_POST['emailid'];
             $phone=$_POST['phone'];
@@ -81,7 +93,31 @@
             $gender=$_POST['gender'];
             $address=$_POST['address'];
             $Aadhar_No=$_POST['Aadhar_No'];
-    
+
+            $filename=$_FILES['file']['name'];
+
+            $destination = 'uploads/' . $filename;
+            $extension = pathinfo($filename, PATHINFO_EXTENSION);
+            
+            $file = $_FILES['file']['tmp_name'];
+            // $size = $_FILES['filename']['size'];
+            if (!in_array($extension, ['zip', 'pdf', 'docx','jpeg','jpg','png']))
+            {
+                echo "You file extension is not allowed";
+            } 
+            move_uploaded_file($file, $destination);
+            // else {
+                // move the uploaded (temporary) file to the specified destination
+            //     if (move_uploaded_file($file, $destination)) {
+            //         $sql = "INSERT INTO customer (filename) VALUES ('$filename')";
+            //         if (mysqli_query($conn, $sql)) {
+            //             echo "File uploaded successfully";
+            //         }
+            //     } else {
+            //         echo "Failed to upload file.";
+            //     }
+            // }
+
             if ($emppass != $empconf)
             {
                 echo("Error... Passwords do not match");
@@ -97,8 +133,8 @@
                 // $sql = "CALL insert_cust('$emp','$emppass','$emailid','$phone','$age');";
 //                 INSERT INTO table_name (column1, column2, column3, ...)
 // VALUES (value1, value2, value3, ...);
-                $sql="INSERT INTO Customer (User_Name,Pswd,Email,Gender,Phone,Age,Aadhar_No,Address) 
-                VALUES('$emp','$emppass','$emailid','$gender','$phone','$age','$Aadhar_No','$address'); ";
+                $sql="INSERT INTO Customer (User_Name,Pswd,Email,Gender,Phone,Age,Aadhar_No,Address,name) 
+                VALUES('$emp','$emppass','$emailid','$gender','$phone','$age','$Aadhar_No','$address','$filename'); ";
                 //$sql="INSERT INTO Customer VALUES('$emp','$emppass','$emailid','$phone','$age');";  
                 //$sql.="INSERT INTO airport(airportid) VALUES('$aid');";
   
